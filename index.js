62e370554bc6ca1386414728bc1cf03c2f1caa4f
mscamposmg <mscamposmg@gmail.com>
2023-07-24 19:21:44 +1000

    teste

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-fetch()
-.then(Response =>
-    Response.OK ?
-    Response.json() :
-    { message: "Something went wrong"}
-    )
+// fetch()
+// .then(Response =>
+//     Response.OK ?
+//     Response.json() :
+//     { message: "Something went wrong"}
+//     )
 
 inquirer
   .prompt([
     {
       type: 'input',
-      name: 'name',
+      name: 'Name of the Project',
       message: 'What is the name of your Project?',
     },
     {
       type: 'checkbox',
       message: 'Select what informations will your Project provide?',
-      name: 'stack',
+      name: 'Informations',
       choices: ['DESCRIPTION', 'INSTALLATION', 'USAGE', 'CONTRIBUTING', 'TESTS'],
     },
-    // {
-    //   type: 'list',
-    //   message: 'What is your preferred method of communication?',
-    //   name: 'contact',
-    //   choices: ['email', 'phone', 'telekinesis'],
-    // },
+    {
+      type: 'list',
+      message: 'Choose one license of the list below?',
+      name: 'License',
+      choices: ['ISC', 'MIT', 'Mozilla Public License 2.0', 'Microsoft Public License'],
+    },
   ])
   .then((data) => {
     const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
