469b8f556dc46a4069fe258ef5ba381c936484ea
mscamposmg <mscamposmg@gmail.com>
2023-07-24 19:40:54 +1000

    testes

--- index.js
+++ index.js
@@ -12,26 +12,26 @@ inquirer
   .prompt([
     {
       type: 'input',
-      name: 'Name of the Project',
+      name: 'name',
       message: 'What is the name of your Project?',
     },
     {
       type: 'checkbox',
       message: 'Select what informations will your Project provide?',
-      name: 'Informations',
+      name: 'information',
       choices: ['DESCRIPTION', 'INSTALLATION', 'USAGE', 'CONTRIBUTING', 'TESTS'],
     },
     {
       type: 'list',
       message: 'Choose one license of the list below?',
-      name: 'License',
+      name: 'license',
       choices: ['ISC', 'MIT', 'Mozilla Public License 2.0', 'Microsoft Public License'],
     },
   ])
-  .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+.then((data) => {
+  const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
-  });
+  fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+  );
+});
