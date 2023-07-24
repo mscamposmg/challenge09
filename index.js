75afd33c1590832696c2aad0fb74e6c384e35083
mscamposmg <mscamposmg@gmail.com>
2023-07-24 18:40:50 +1000

    creating a prompt

--- index.js
+++ index.js
@@ -1,50 +1,37 @@
-// console.log("hellooooo, Node!!!");
+const inquirer = require('inquirer');
+const fs = require('fs');
 
-// var dog = {
-//     name: "Lassie",
-//     sound: "woof!",
-//     makesound: () => console.log(this.sound),
-//     readTag: () => console.log("The dog's tag reads: " + this.name + ".")
-// };
+fetch()
+.then(Response =>
+    Response.OK ?
+    Response.json() :
+    { message: "Something went wrong"}
+    )
 
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your Project?',
+    },
+    {
+      type: 'checkbox',
+      message: 'Select what informations will your Project provide?',
+      name: 'stack',
+      choices: ['DESCRIPTION', 'INSTALLATION', 'USAGE', 'CONTRIBUTING', 'TESTS'],
+    },
+    // {
+    //   type: 'list',
+    //   message: 'What is your preferred method of communication?',
+    //   name: 'contact',
+    //   choices: ['email', 'phone', 'telekinesis'],
+    // },
+  ])
+  .then((data) => {
+    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-
-// var dog = {
-//     name: "Lassie",
-//     sound: "woof!",
-//     makesound: function () {
-//         console.log(this.sound)
-//         setTimeout(() => console.log(this), 2000)
-//     },
-//     readTag: function () {
-//         console.log("The dog's tag reads: " + this.name + ".")
-//     }
-// };
-
-// var dog = {
-//     name: "Lassie",
-//     sound: "woof!",
-//     makesound() {
-//         console.log(this.sound)
-//         setTimeout(() => console.log(this), 2000)
-//     },
-//     readTag() {
-//         console.log("The dog's tag reads: " + this.name + ".")
-//     }
-// };
-
-// dog.makesound();
-
-// dog.readTag();
-
-const moviePatrons = [
-    {name: "Tom", age: 16},
-    {name: "Alice", age: 31},
-    {name: "Aghata", age: 2},
-    {name: "Pablo", age: 63},
-    {name: "Simon", age: 58},
-    {name: "Edwards", age: 42},
-    {name: "John", age: 17}
-];
-
-moviePatrons.forEach(patron => console.log("Thank you for your help " + patron.name));
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+      err ? console.log(err) : console.log('Success!')
+    );
+  });
