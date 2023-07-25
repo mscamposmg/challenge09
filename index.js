10d16f452776048e717f461a2368ca139f4f611d
mscamposmg <mscamposmg@gmail.com>
2023-07-25 11:28:35 +1000

    test

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// fetch()
-// .then(Response =>
-//     Response.OK ?
-//     Response.json() :
-//     { message: "Something went wrong"}
-//     )
+// const generateReadme = ({ information, contribution, guidelines, test }) =>
+//   `# Title of the Project: ${name}
+  
+//   ## ${description}`
 
 inquirer
   .prompt([
@@ -16,22 +14,56 @@ inquirer
       message: 'What is the name of your Project?',
     },
     {
-      type: 'checkbox',
-      message: 'Select what informations will your Project provide?',
-      name: 'information',
-      choices: ['DESCRIPTION', 'INSTALLATION', 'USAGE', 'CONTRIBUTING', 'TESTS'],
+      type: 'input',
+      name: 'description',
+      message: 'Write a description of your Project:',
     },
-    {
-      type: 'list',
-      message: 'Choose one license of the list below?',
-      name: 'license',
-      choices: ['ISC', 'MIT', 'Mozilla Public License 2.0', 'Microsoft Public License'],
-    },
+    // {
+    //   type: 'input',
+    //   name: 'name',
+    //   message: 'What is the name of your Project?',
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'name',
+    //   message: 'What is the name of your Project?',
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'name',
+    //   message: 'What is the name of your Project?',
+    // },
+    // {
+    //   type: 'input',
+    //   name: 'name',
+    //   message: 'What is the name of your Project?',
+    // },
+
+
+    // {
+    //   type: 'confirm',
+    //   message: 'Select what informations will your Project provide?',
+    //   name: 'information',
+    //   choices: ['DESCRIPTION', 'INSTALLATION', 'USAGE', 'CONTRIBUTING', 'TESTS'],
+    // },
+    // {
+    //   type: 'list',
+    //   message: 'Choose one license of the list below?',
+    //   name: 'license',
+    //   choices: ['ISC', 'MIT', 'Mozilla Public License 2.0', 'Microsoft Public License'],
+    // },
   ])
 .then((data) => {
   const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
 
-  fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+  // fs.writeFile('README.md', process.argv[2], (err) =>
+  // err ? console.error(err) : console.log('Commit Logged!')
+  // );
+
+  // fs.writeFile('README.md', `${process.argv[2]}\n`, (err) =>
+  // err ? console.error(err) : console.log('Commit Logged!')
+  // );
+  fs.writeFile(text.md, JSON.stringify(data, null, '\t'), (err) =>
     err ? console.log(err) : console.log('Success!')
   );
 });
